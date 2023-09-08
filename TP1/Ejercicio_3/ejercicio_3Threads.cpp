6a0b38414cf401197f80d7d209476401f7304c4c
facu Gaviola <facundo.gaviola@gmail.com>
2023-09-08 12:16:35 -0300

    Ejercicio 3 con hilos

--- TP1/Ejercicio_3/ejercicio_3Threads.cpp
+++ TP1/Ejercicio_3/ejercicio_3Threads.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 
-// Función para multiplicar dos matrices NxN
-vector<vector<float>> multiplicarMatrices(const vector<vector<float>>& matrizA, const vector<vector<float>>& matrizB) {
+// Función para multiplicar un grupo de filas de las matrices A y B y almacenar el resultado en la matriz resultado
+void multiplicarFilas(const vector<vector<float>>& matrizA, const vector<vector<float>>& matrizB, vector<vector<float>>& matrizResultado, int inicio, int fin) {
     int N = matrizA.size();
-    vector<vector<float>> resultado(N, vector<float>(N, 0.0));
-
-    for (int i = 0; i < N; i++) {
+    for (int i = inicio; i < fin; i++) {
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < N; k++) {
-                resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+                matrizResultado[i][j] += matrizA[i][k] * matrizB[k][j];
             }
         }
     }
-
-    return resultado;
 }
 
 //Imprime las 4 esqinas de una matriz
@@ -45,6 +43,7 @@ void sumatoriaMatriz(const vector<vector<float>>& matriz) {
 
 int main() {
     int N;
+    int numThreads = 10;
 
     cout << "Ingrese el valor de N (tamaño de las matrices NxN): ";
     cin >> N;
@@ -52,9 +51,26 @@ int main() {
     // Crear matrices NxN y llenarlas con valores
     vector<vector<float>> matrizA(N, vector<float>(N, 0.1));  // Llenar matrizA con 0.1
     vector<vector<float>> matrizB(N, vector<float>(N, 0.2));  // Llenar matrizB con 0.2
+    vector<vector<float>> matrizResultado(N, vector<float>(N, 0.0));  // Matriz de resutados 
 
-    // Multiplicar las matrices
-    vector<vector<float>> matrizResultado = multiplicarMatrices(matrizA, matrizB);
+    // Crear vector de hilos
+    vector<thread> threads;
+
+    // Dividir el trabajo entre los hilos
+    int filasPorHilo = N / numThreads;
+    int inicio = 0;
+    int fin = filasPorHilo;
+
+    for (int i = 0; i < numThreads; i++) {
+        threads.emplace_back(multiplicarFilas, ref(matrizA), ref(matrizB), ref(matrizResultado), inicio, fin);
+        inicio = fin;
+        fin += filasPorHilo;
+    }
+
+    // Esperar a que todos los hilos terminen
+    for (auto& thread : threads) {
+        thread.join();
+    }
 
     // Mostrar la matriz resultado
     cout << "Matriz resultado de la multiplicación:" << endl;
